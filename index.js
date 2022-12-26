3ad03611db9222dd4fcca95caa611d80a09e020f
magnus346 <theo@slynt.com>
2022-12-26 12:22:39 +0100

    initial

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
-const express = require('express');
-const app = express();
-const http = require('http');
-const server = http.createServer(app);
+const express = require('express')
+
+const app = express()
+const PORT = 4000
+
+app.listen(PORT, () => {
+  console.log(`API listening on PORT ${PORT} `)
+})
 
 app.get('/', (req, res) => {
-  res.send('<h1>Hello world</h1>');
-});
+  res.send('Hey this is my API running 🥳')
+})
 
-server.listen(3000, () => {
-  console.log('listening on *:3000');
-});
+app.get('/about', (req, res) => {
+  res.send('This is my about route..... ')
+})
+
+// Export the Express API
+module.exports = app
