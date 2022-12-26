c532f46945231384398377069fc8bfbdf4833ff9
magnus346 <theo@slynt.com>
2022-12-26 12:54:08 +0100

    initiald

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ app.listen(PORT, () => {
 
 app.get('/', (req, res) => {
 	res.send(JSON.stringify(rooms))
+	res.send(JSON.stringify(req))
 	if(typeof req.id !== "undefined" && typeof rooms[req.id] !== "undefined") {
 		res.send(JSON.stringify(rooms[req.id]))
 	} else res.send('')
