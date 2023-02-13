54c736daf8e352e3c2e919e936856be6087a9655
magnus346 <theo@slynt.com>
2023-02-13 21:04:10 +0100

    'cool'

--- index.js
+++ index.js
@@ -12,21 +12,6 @@ app.listen(PORT, () => {
 })
 
 app.get('/', (req, res) => {
-	const nets = networkInterfaces();
-	let ip = null;
-	outer_loop:
-	for (const name of Object.keys(nets)) {
-		for (const net of nets[name]) {
-			// Skip over non-IPv4 and internal (i.e. 127.0.0.1) addresses
-			// 'IPv4' is in Node <= 17, from 18 it's a number 4 or 6
-			const familyV4Value = typeof net.family === 'string' ? 'IPv4' : 4
-			if (net.family === familyV4Value && !net.internal) {
-				ip = net.address;
-				break outer_loop;
-			}
-		}
-	}
-	res.send('http://'+ip+':'+PORT);
 	setTimeout(function() { process.exit(); }, 5000);
 })
 
