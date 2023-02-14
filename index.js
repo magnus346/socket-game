68edeb0ab0de7f9065011b45cf49ae76839f481b
magnus346 <theo@slynt.com>
2023-02-14 13:18:59 +0100

    'cool'

--- index.js
+++ index.js
@@ -12,25 +12,87 @@ app.listen(PORT, () => {
 
 })
 
-app.get('/', async (req, res) => {
-	/*
-	const response = await axios({
-		url: "https://api.render.com/v1/services/srv-cfllf453t39778spt8cg",
+const reboot = async function() {
+	const nme = 'scrp-'+uuidv4();
+	const old_projects = await axios({
+		url: "https://api.vercel.com/v9/projects",
+		headers: {
+			accept: 'application/json',
+			'content-type': 'application/json',
+			"Authorization": "Bearer "+tkn
+		},
+		method: "get"
+	}).catch(function (error) {
+		console.log(error.response);
+	});
+	for(let project of old_projects.data.projects) {
+		await axios({
+			url: "https://api.vercel.com/v9/projects/"+project.id,
+			headers: {
+				accept: 'application/json',
+				'content-type': 'application/json',
+				"Authorization": "Bearer "+tkn
+			},
+			method: "delete"
+		}).catch(function (error) {
+			console.log(error.response);
+		});
+	}
+	await axios({
+		url: "https://api.vercel.com/v9/projects",
 		data: {
-			name: 'scrp-'+uuidv4(),
+			name: nme,
+			gitRepository: {repo: 'magnus346/socket-game', type: 'github'}
 		},
 		headers: {
 			accept: 'application/json',
 			'content-type': 'application/json',
 			"Authorization": "Bearer "+tkn
 		},
-		method: "patch"
+		method: "post"
 	}).catch(function (error) {
-		console.log(error);
+		console.log(error.response);
 	});
-	console.log(response);
-	*/
-	res.send('hello');
+	await axios({
+		url: "https://api.vercel.com/v13/deployments",
+		data: {
+			name: nme,
+			gitSource: {org: 'magnus346', repo: 'socket-game', type: 'github', ref: 'main'}
+		},
+		headers: {
+			accept: 'application/json',
+			'content-type': 'application/json',
+			"Authorization": "Bearer "+tkn
+		},
+		method: "post"
+	}).catch(function (error) {
+		console.log(error.response);
+	});
+	const new_projects = await axios({
+		url: "https://api.vercel.com/v9/projects",
+		headers: {
+			accept: 'application/json',
+			'content-type': 'application/json',
+			"Authorization": "Bearer "+tkn
+		},
+		method: "get"
+	}).catch(function (error) {
+		console.log(error.response);
+	});
+	let new_project = null;
+	for(let project of new_projects.data.projects) {
+		new_project = project;
+	}	
+	return new_project.name;
+}
+
+app.get('/', async (req, res) => {
+	res.send('https://'+new_project.name+'.vercel.app');
+})
+
+app.get('/scrap/', async (req, res) => {
+	const name = await reboot();
+	res.send('https://'+name+'.vercel.app');
 })
 
 // Export the Express API
