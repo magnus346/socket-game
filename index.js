2c50f509e3663a558712fe47bd69357aeba8a442
magnus346 <theo@slynt.com>
2023-02-14 13:47:52 +0100

    'cool'

--- index.js
+++ index.js
@@ -2,6 +2,42 @@ const express = require('express')
 const { v4: uuidv4 } = require('uuid');
 const { networkInterfaces } = require('os');
 const axios = require('axios');
+const cheerio = require("cheerio");
+const unirest = require("unirest");
+
+const selectRandomUserAgent = () => {
+    const userAgents = ["Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36", "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.121 Safari/537.36", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.157 Safari/537.36", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.45 Safari/537.36", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.71 Safari/537.36", ]
+    var randomNumber = Math.floor(Math.random() * userAgents.length);
+    return userAgents[randomNumber];
+}
+
+const scrap = async() => {
+	const url = "https://www.google.com/search?q=accropode&gl=fr&lr=lang_fr&hl=lang_fr&start=10";
+
+	return unirest
+	.get(url)
+	.headers({
+		"User-Agent":
+		selectRandomUserAgent()
+	})
+	.then((response) => {
+		let $ = cheerio.load(response.body);
+
+		let results = [];
+		
+		if($(".g-recaptcha").length)
+			throw new Error('Recaptcha');
+		else {
+			$(".g").each((i,el) => {
+				results.push({
+					title: $(el).find("h3").text(),
+					link: $(el).find("a").attr("href")
+				})
+			})
+			return results;
+		}
+	})
+}
 
 const app = express()
 const PORT = 3000
@@ -9,7 +45,7 @@ const PORT = 3000
 const tkn = 'x6nzGDEq4havOYnRtt5q6hnP';
 
 app.listen(PORT, () => {
-
+	
 })
 
 const reboot = async function() {
@@ -91,8 +127,13 @@ app.get('/', async (req, res) => {
 })
 
 app.get('/scrap/', async (req, res) => {
-	const name = await reboot();
-	res.send('https://'+name+'.vercel.app');
+	try {
+		const results = await scrap();
+		res.send(results);
+	} catch (e) {
+		const name = await reboot();
+		res.send({reboot: 'https://'+name+'.vercel.app'});		
+	}
 })
 
 // Export the Express API
